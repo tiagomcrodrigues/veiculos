0c01358be7fc223c2095f1ca5e8e7ea5410e059a
Tiago Rodrigues <tiago@ASGARD.LOCAL>
2023-12-15 19:14:33 -0300

    patch

---
+++ src/Veiculos/Veiculos.API/Models/Request/VeiculoPatchRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace Veiculos.API.Models.Request
+{
+    public class VeiculoPatchRequest
+    {
+        public short? AnoModelo { get; set; }
+
+        public short? AnoFabricacao { get; set; }
+
+        public string? Modelo { get; set; }
+        
+        public string? Fabricante { get; set; }
+        
+        public string? Cor { get; set; }
+        
+        public string? Placa { get; set; }
+        
+        public string? Tipo { get; set; }
+
+    }
+}
