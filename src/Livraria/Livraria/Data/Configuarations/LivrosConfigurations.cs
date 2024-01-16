114cfa7acb5c99ec899252b8c86b75f2aff91026
Tiago Rodrigues <tiagomcoutorodrigues@gmail.com>
2024-01-16 20:02:48 -0300

    autoresConfiguratios

--- src/Livraria/Livraria/Data/Configuarations/LivrosConfigurations.cs
+++ src/Livraria/Livraria/Data/Configuarations/LivrosConfigurations.cs
@@ -1,6 +1,60 @@
-﻿namespace Livraria.Data.Configuarations
+﻿using Livraria.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Livraria.Data.Configuarations
 {
-    public class LivrosConfigurations
+    public class LivrosConfigurations : IEntityTypeConfiguration<Livros>
     {
+        public void Configure(EntityTypeBuilder<Livros> b)
+        {
+            b.ToTable(nameof(Livros));
+
+            b.HasKey(nameof(Livros.Id));
+
+            b.Property(nameof(Livros.Id))
+                .ValueGeneratedOnAdd();
+
+            b.Property(b => b.Titulo)
+                .HasColumnName(nameof(Livros.Titulo))
+                .HasMaxLength(80)
+                .IsUnicode(false)
+                .IsRequired();
+
+            b.Property(b => b.Descricao)
+                .HasColumnName(nameof(Livros.Descricao))
+                .HasMaxLength(500)
+                .IsUnicode(false)
+                .IsRequired();
+
+            b.Property(b => b.AutorId)
+                .HasColumnName(nameof(Livros.AutorId))
+                .IsRequired();
+
+            b.Property(b => b.Quantidade)
+                .HasColumnName(nameof(Livros.Quantidade))
+                .IsRequired();
+
+            b.Property(b => b.QuantidadeEmprestimo)
+                .HasColumnName(nameof(Livros.QuantidadeEmprestimo))
+                .IsRequired();
+
+            b.Property(b => b.PrmitirEmprestimo)
+                .HasColumnName(nameof(Livros.PrmitirEmprestimo))
+                .HasColumnType("bit")
+                .HasDefaultValue(false)
+                .IsRequired();
+
+            b.HasIndex(Livros => Livros.Titulo)
+                .HasDatabaseName($"UK_{nameof(Livros)}_{nameof(Livros.Titulo)}")
+                .IsUnique();
+
+            b.HasOne(o => o.Autores)
+                .WithMany(b => b.Livros)
+                .HasForeignKey(fk => fk.AutorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+        }
     }
 }
--- src/Livraria/Livraria/Data/Entities/Livros.cs
+++ src/Livraria/Livraria/Data/Entities/Livros.cs
@@ -1,23 +1,24 @@
-﻿//namespace Livraria.Data.Entities
-//{
-//    public class Livros
-//    {
-       
+﻿namespace Livraria.Data.Entities
+{
+    public class Livros
+    {
 
-//        public Livros(int id)
-//        {
-//            Id = id;
-//        }
 
-//        public int Id { get; set; }
+        public Livros(int id)
+        {
+            Id = id;
+        }
 
-//        public string Descricao { get; set; }
-//        public int AutorId { get; set; }
-//        public int Quantidade { get; set; }
-//        public int QuantidadeEmprestimo { get; set; }
-//        public bool PrmitirEmprestimo { get; set; }
+        public int Id { get; set; }
 
-//        public virtual  Autores Autores { get; set; }
-//        public virtual ICollection<Emprestimos> Emprestadores { get; set;}
-//    }
-//}
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public int AutorId { get; set; }
+        public int Quantidade { get; set; }
+        public int QuantidadeEmprestimo { get; set; }
+        public bool PrmitirEmprestimo { get; set; }
+
+        public virtual Autores Autores { get; set; }
+        //public virtual ICollection<Emprestimos> Emprestadores { get; set; }
+    }
+}
